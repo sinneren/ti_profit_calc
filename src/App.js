9dd920896cad700656c09e634a52674e2f87d8e2
Serj <regieslinkas@gmail.com>
2018-12-12 17:33:20 +0500

    calc complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0,
+    summary_buy: 0,
+    summary: 0,
+    price: 0,
+    comission: 99,
+    tax: 0.3,
+    double_comission: true,
+  };
+  calculate = () => {
+    let modifier = this.state.double_comission ? 2 : 1;
+    let price_count = parseFloat(this.state.price * this.state.count);
+    let price_after_buying = price_count - parseFloat((price_count * this.state.tax / 100) * 2) - parseFloat(this.state.comission * modifier);
+    let price_zero = price_count - price_after_buying + price_count;
+
+    this.setState({
+      summary_buy: price_after_buying,
+      summary: price_zero,
+    });
+  }
+  onChangeHandler = (event) => {
+    let inputName = event.currentTarget.name;
+    let inputVal = event.currentTarget.value;
+
+    this.setState({
+      [inputName]: (inputName === "double_comission") ? !this.state.double_comission : inputVal,
+    });
+  }
+  submitHandler = (event) => {
+    event.preventDefault();
+
+    this.calculate()
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Калькулятор точки безубыточности для Тинькофф.Инвестиций</h1>
+          <input onChange={this.onChangeHandler} name="price" placeholder="price" />
+          <input onChange={this.onChangeHandler} name="count" placeholder="count" />
+          <input onChange={this.onChangeHandler} name="tax" placeholder="tax" value={this.state.tax} />
+          <input onChange={this.onChangeHandler} name="comission" placeholder="comission" value={this.state.comission} />
+          <label><small>Двойная комиссия</small> <input type="checkbox" name="double_comission" checked={this.state.double_comission} onChange={this.onChangeHandler} /></label>
+          <button onClick={this.submitHandler}>Посчитать</button>
+          <div>{this.state.summary_buy + " [" + this.state.summary + "]"}</div>
         </header>
       </div>
     );
